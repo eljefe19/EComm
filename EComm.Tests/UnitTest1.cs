48fe2043f5bb06a1aad879b10b788866060bde67
Jeffrey Somera <el_jefe77@comcast.net>
2019-05-09 14:32:01 -0700

    Second commit

---
+++ EComm.Tests/UnitTest1.cs
@@ -0,0 +1,71 @@
+using EComm.DataAccess;
+using EComm.Model;
+using EComm.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EComm.Tests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        [Theory]
+        [InlineData(0, 0, 0 )]
+        [InlineData(10, 6, 4)]
+        public void PassingTest(int a, int b, int c)
+        {
+            Assert.Equal(a, (b + c));
+        }
+
+        [Fact]
+        public void ProductDetails()
+        {
+            // Arrange
+            var controller = new ProductController(CreateStubContext());
+
+            // Act
+            var result = controller.Detail(2);
+
+            // Assert
+            Assert.IsAssignableFrom<ViewResult>(result);
+            var vr = result as ViewResult;
+            Assert.IsAssignableFrom<Product>(vr.Model);
+            var model = vr.Model as Product;
+            Assert.Equal("Bread", model.ProductName);
+        }
+
+        private ECommDataContext CreateStubContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ECommDataContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDb");
+            var context = new ECommDataContext(optionsBuilder.Options);
+
+            // Add sample data
+            context.Products.Add(new Product
+            {
+                Id = 1,
+                ProductName = "Milk",
+                UnitPrice = 2.50M
+            });
+            context.Products.Add(new Product
+            {
+                Id = 2,
+                ProductName = "Bread",
+                UnitPrice = 3.25M,
+                SupplierId = 1
+            });
+            context.Products.Add(new Product
+            {
+                Id = 3,
+                ProductName = "Juice",
+                UnitPrice = 5.75M
+            });
+            context.Suppliers.Add(new Supplier { Id = 1, CompanyName = "Acme" });
+
+            context.SaveChanges();
+            return context;
+        }
+    }
+}
